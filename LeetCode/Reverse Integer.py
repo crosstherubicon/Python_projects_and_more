ca2d1def5b45f4875738ae7b6d5947bec97c9d31
Sans Basnet <43323395+SansBasnet@users.noreply.github.com>
2020-10-10 16:49:53 -0500

    Create Reverse Integer.py

---
+++ LeetCode/Reverse Integer.py
@@ -0,0 +1,45 @@
+'''Given a 32-bit signed integer, reverse digits of an integer.
+
+Note:
+Assume we are dealing with an environment that could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+
+ 
+
+Example 1:
+
+Input: x = 123
+Output: 321
+Example 2:
+
+Input: x = -123
+Output: -321
+Example 3:
+
+Input: x = 120
+Output: 21
+Example 4:
+
+Input: x = 0
+Output: 0
+ 
+
+Constraints:
+
+-231 <= x <= 231 - 1
+'''
+
+#Solution for Python 3
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        if x >= 2**31-1 or x <= -2**31: return 0
+        else:
+            strg = str(x)
+            if x >= 0 :
+                revst = strg[::-1]
+            else:
+                temp = strg[1:] 
+                temp2 = temp[::-1] 
+                revst = "-" + temp2
+            if int(revst) >= 2**31-1 or int(revst) <= -2**31: return 0
+            else: return int(revst)
