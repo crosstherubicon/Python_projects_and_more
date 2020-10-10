c5552434f3c90c2255bf86a4c9dce93aa20bd4c0
Sans Basnet <43323395+SansBasnet@users.noreply.github.com>
2020-10-10 16:44:48 -0500

    Create q1.py

---
+++ LeetCode/q1.py
@@ -0,0 +1,8 @@
+def twoSum(self, nums, target):
+        seen = {}
+        for i, v in enumerate(nums):
+            remaining = target - v
+            if remaining in seen:
+                return [seen[remaining], i]
+            seen[v] = i
+        return []
