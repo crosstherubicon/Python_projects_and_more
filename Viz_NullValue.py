6fd716dbe23fbf2b9b9408f9f7fef5390485c138
Sans Basnet <43323395+SansBasnet@users.noreply.github.com>
2021-04-28 14:12:25 -0500

    Create Viz_NullValue.py

---
+++ Viz_NullValue.py
@@ -0,0 +1,12 @@
+%matplotlib inline
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+df = pd.read_csv('data.csv')
+df.head()
+
+plt.figure(figsize=(15,8))
+
+sns.heatmap(df.isnull(),yticklabels=False,
+            cbar=False, cmap='viridis') #creating heatmap with nullvalues
